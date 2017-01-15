a0d83dfd7fc1b19ab5168292dd77ff45d449351b
Ani Sledge <ani.sledge@gmail.com>
2017-01-14 23:11:25 -0700

    Test signup POST handling and validation
    Uses tests to verify that the signup handler is dealing with
    valid and invalid form input correctly.
    
    * Creates test user input data
    * Tests detect_errors against invalid data
    * Submits the form with invalid data
    * Tests that the signup template has been rerendered
    * Tests that the username and email, but not password fields
      are populated
    * Submits the form with valid data
    * Tests for a 302 redirect to the user page

--- main_test.py
+++ main_test.py
@@ -12,6 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#TEST DATA
+signup_valid = { 'username': 'test', 'password': 'test1234', 'verify': 'test1234', 'email': 'test@test.com'}
+signup_invalid = { 'username': '">ha', 'password': '', 'verify': 'test123', 'email': 'test' }
+signup_error = { 'username': True, 'password': True, 'verify': True, 'email': True}
+login_valid = { 'username': 'test', 'password': 'test1234' }
+login_invalid = { 'username': '', 'password': '' }
+
 import webtest
 import blog
 import os
@@ -41,9 +48,45 @@ def test_get():
     logout = logout.follow()
     assert logout.body == template('signup.html')
     
-    user = app.get('/user')
+    user = app.get('/user?username=test')
     assert user.status_int == 200
-    assert user.body == template('user.html')
+    assert user.body == template('user.html', username='test')
 	
+    #SIGNUP FORM
+    signup_form = signup.form
+    assert signup_form.method == 'POST'
+    
+    assert signup_error, False == blog.detect_errors(signup_invalid['username'], signup_invalid['password'], 
+        signup_invalid['verify'], signup_invalid['email'])
+
+    #INVALID
+    signup_form['username'] = signup_invalid['username']
+    signup_form['password'] = signup_invalid['password']
+    signup_form['verify'] = signup_invalid['verify']
+    signup_form['email'] = signup_invalid['email']
+    invalid_signup = signup_form.submit()
+
+    error_form = template("signup.html", error=signup_error, username=signup_invalid['username'], email=signup_invalid['email'])
+    assert invalid_signup.body == error_form
+    
+    signup_form = invalid_signup.form
+    assert signup_form['username'].value == signup_invalid['username']
+    assert signup_form['password'].value == ""
+    assert signup_form['verify'].value == ""
+    assert signup_form['email'].value == signup_invalid['email']
+
+    #VALID
+    signup_form['username'] = signup_valid['username']
+    signup_form['password'] = signup_valid['password']
+    signup_form['verify'] = signup_valid['verify']
+    signup_form['email'] = signup_valid['email']
+    valid_signup = signup_form.submit()
+    
+    assert valid_signup.status_int == 302
+    valid_signup = valid_signup.follow()
+    assert valid_signup.body == template('user.html', username=signup_valid['username'])
+
+    #LOGIN FORM
+
 test_get()
 	
