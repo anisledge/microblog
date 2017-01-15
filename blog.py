364bd64db04202beeb22cb03a8befb2439d39c60
Ani Sledge <ani.sledge@gmail.com>
2017-01-14 20:11:57 -0700

    User authentication pages respond to GET requests
    * Edited Handler.render function to accept a string input
    * Added calls to render in signup, login, logout,
      and user get handlers
    * Passes all main_test.py routing tests

--- blog.py
+++ blog.py
@@ -1,30 +1,30 @@
 import webapp2 
 
 class Handler(webapp2.RequestHandler):
-	def render(self, **params):
-		self.response.out.write(params)
+	def render(self, string):
+		self.response.out.write(string)
 
 class Signup(Handler):
 	def get(self):
-		pass
+		self.render('User signup')
 
 	def post(self):
 		pass
 
 class Login(Handler):
 	def get(self):
-		pass
+		self.render('User login')
 
 	def post(self):
 		pass
 
 class Logout(Handler):
 	def get(self):
-		pass
+		self.render('User logout')
 
 class UserHandler(Handler):
 	def get(self):
-		pass
+		self.render('User home')
 
 app = webapp2.WSGIApplication([('/signup', Signup),
 							('/login', Login),
