dcf9c50e707e6dcfcba64be37a6c7187723f7421
Ani Sledge <ani.sledge@gmail.com>
2017-01-15 19:20:42 -0700

    Bug: Fixed BadArgumentError thrown in main_test.py
    'BadArgumentError: app must not be empty' was thrown in the test
    file after database access was added to the application. Using
    unittest creates a temporary datastore that can be accessed for
    testing purposes.
    
    * Import testbed and datastore from google appengine
    * Create a unittest TestCase for the app
    * Activate & Deactivate the testbed in setUp and tearDown
    * Add test functions for each handler
    
    Read More:
    https://cloud.google.com/appengine/docs/python/tools/handlertesting

--- main_test.py
+++ main_test.py
@@ -1,105 +1,142 @@
+#Avoid import errors: add google directories to the path
 import sys
+sys.path.insert(1, "/Users/Ani/Downloads/google-cloud-sdk/lib/third_party")
+sys.path.insert(1, "/Users/Ani/Downloads/google-cloud-sdk/platform/google_appengine")
 
-sys.path.append("/Users/Ani/Downloads/google-cloud-sdk/platform/google_appengine")
-sys.path.append("/Users/Ani/Downloads/google-cloud-sdk/lib/third_party")
-
+#Import necessary files for testing
+from google.appengine.ext import testbed
+from google.appengine.api import datastore
+import unittest
 import webtest
 import blog
+
+#Setup jinja2 templates
 import os
 import jinja2
-
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-    autoescape = True)
-
-#TEST DATA
-signup_valid = { 'username': 'test', 'password': 'test1234', 'verify': 'test1234', 'email': 'test@test.com'}
-signup_invalid = { 'username': '">ha', 'password': '', 'verify': 'test123', 'email': 'test' }
-signup_error = { 'username': True, 'password': True, 'verify': True, 'email': True}
-login_valid = { 'username': 'test', 'password': 'test1234' }
-login_invalid = { 'username': '', 'password': '' }
+   autoescape = True)
 
 def template(template, **params):
     return jinja_env.get_template(template).render(params)
 
-def test_get():
-    app = webtest.TestApp(blog.app)
+#Test class
+class AppTest(unittest.TestCase):
+    def setUp(self):
+        self.testapp = webtest.TestApp(blog.app)
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
 
-    #USER ROUTING
-    signup = app.get('/signup')
-    assert signup.status_int == 200
-    assert signup.body == template('signup.html')
+        #Test User
+        pw_hash = '472f43f061676da04375012af4b9ae48d87b2f6fbedb4f9f64e8923d24b6e391|gLGkF'
+        user = blog.User(username='test1', password=pw_hash, email="", parent=blog.blog_key())
+        user.put()
+        self.user = user
+        self.user_id = str(self.user.key().id())
+        self.password = 'password'
+        
+    def tearDown(self):
+        self.testbed.deactivate()
 
-    login = app.get('/login')
-    assert login.status_int == 200
-    assert login.body == template('login.html')
+    def test_signup_handler(self):
+        response = self.testapp.get('/signup')
+        self.assertEqual(response.status_int, 200)
+        self.assertEqual(response.body, template('signup.html'))
 
-    logout = app.get('/logout')
-    assert logout.status_int == 302
-    logout = logout.follow()
-    assert logout.body == template('signup.html')
-    
-    user = app.get('/user?username=test')
-    assert user.status_int == 200
-    assert user.body == template('user.html', username='test')
-	
-    #SIGNUP FORM
-    signup_form = signup.form
-    assert signup_form.method == 'POST'
-    
-    assert signup_error, False == blog.detect_errors(signup_invalid['username'], signup_invalid['password'], 
-        signup_invalid['verify'], signup_invalid['email'])
+    def test_signup_valid(self):
+        response = self.testapp.get('/signup')
+        form = response.form
+        self.assertEqual(form.method, 'POST')
 
-    #INVALID
-    signup_form['username'] = signup_invalid['username']
-    signup_form['password'] = signup_invalid['password']
-    signup_form['verify'] = signup_invalid['verify']
-    signup_form['email'] = signup_invalid['email']
-    invalid_signup = signup_form.submit()
+        valid_input = { 'username': 'test', 
+                        'password': 'test1234', 
+                        'verify': 'test1234', 
+                        'email': 'test@test.com'}
 
-    error_form = template("signup.html", error=signup_error, username=signup_invalid['username'], email=signup_invalid['email'])
-    assert invalid_signup.body == error_form
-    
-    signup_form = invalid_signup.form
-    assert signup_form['username'].value == signup_invalid['username']
-    assert signup_form['password'].value == ""
-    assert signup_form['verify'].value == ""
-    assert signup_form['email'].value == signup_invalid['email']
+        form['username'] = valid_input['username']
+        form['password'] = valid_input['password']
+        form['verify'] = valid_input['verify']
+        form['email'] = valid_input['email']
+        
+        valid_signup = form.submit()
+        self.assertEqual(valid_signup.status_int, 302)
+        valid_signup = valid_signup.follow()
+        self.assertEqual(valid_signup.body, template('user.html', username=valid_input['username']))
 
-    #VALID
-    signup_form['username'] = signup_valid['username']
-    signup_form['password'] = signup_valid['password']
-    signup_form['verify'] = signup_valid['verify']
-    signup_form['email'] = signup_valid['email']
-    valid_signup = signup_form.submit()
-    
-    assert valid_signup.status_int == 302
-    valid_signup = valid_signup.follow()
-    assert valid_signup.body == template('user.html', username=signup_valid['username'])
+    def test_signup_invalid(self):
+        response = self.testapp.get('/signup')
+        form = response.form
+        self.assertEqual(form.method, 'POST')
 
-    #LOGIN FORM
-    login_form = login.form
-    assert login_form.method == 'POST'
+        invalid_input = { 'username': '">ha', 
+                          'password': '',
+                          'verify': 'test123', 
+                          'email': 'test' }
+        
+        form['username'] = invalid_input['username']
+        form['password'] = invalid_input['password']
+        form['verify'] = invalid_input['verify']
+        form['email'] = invalid_input['email']
+        
+        invalid_signup = form.submit()
 
-    #INVALID
-    login_form['username'] = login_invalid['username']
-    login_form['password'] = login_invalid['password']
-    invalid_login = login_form.submit()
+        error = { 'username': True, 'password': True, 'verify': True, 'email': True}
 
-    error_form = template('login.html', error=True, username=login_invalid['username'])
-    assert invalid_login.body == error_form
+        error_form = template("signup.html", error=error, username=invalid_input['username'], email=invalid_input['email'])
+        self.assertEqual(invalid_signup.body, error_form)
+        
+        form = invalid_signup.form
+        self.assertEqual(form['username'].value, invalid_input['username'])
+        self.assertEqual(form['password'].value, "")
+        self.assertEqual(form['verify'].value, "")
+        self.assertEqual(form['email'].value, invalid_input['email'])
 
-    login_form = invalid_login.form
-    assert login_form['username'].value == login_invalid['username']
-    assert login_form['password'].value == ""
-    
-    #VALID
-    login_form['username'] = login_valid['username']
-    login_form['password'] = login_valid['password']
-    valid_login = login_form.submit()
-    
-    assert valid_login.status_int == 302
-    valid_login = valid_login.follow()
-    assert valid_login.body == template('user.html', username=login_valid['username'])
+    def test_login_handler(self):
+        response = self.testapp.get('/login')
+        self.assertEqual(response.status_int, 200)
+        self.assertEqual(response.body, template('login.html'))
 
-test_get()
+    def test_login_valid(self):
+        response = self.testapp.get('/login')
+        form = response.form
+        self.assertEqual(form.method, 'POST')
+
+        form['username'] = self.user.username
+        form['password'] = self.password
+        
+        valid_login = form.submit()
+        self.assertEqual(valid_login.status_int, 302)
+        valid_login = valid_login.follow()
+        self.assertEqual(valid_login.body, template('user.html', username=self.user.username))
+
+    def test_login_invalid(self):
+        response = self.testapp.get('/login')
+        form = response.form
+        self.assertEqual(form.method, 'POST')
+
+        form['username'] = "nope"
+        form['password'] = ""
+        
+        invalid_login = form.submit()
+
+        error_form = template('login.html', error=True, username="nope")
+        self.assertEqual(invalid_login.body, error_form)
+
+        form = invalid_login.form
+        self.assertEqual(form['username'].value, "nope")
+        self.assertEqual(form['password'].value, "")
+
+    def test_logout_handler(self):
+        response = self.testapp.get('/logout')
+        self.assertEqual(response.status_int, 302)
+        response = response.follow()
+        self.assertEqual(response.body, template('signup.html'))
+
+    def test_user_handler(self):
+        response = self.testapp.get('/user/' + self.user_id)
+        self.assertEqual(response.status_int, 200)
+        self.assertEqual(response.body, template('user.html', username='test1'))
+
+suite = unittest.TestLoader().loadTestsFromTestCase(AppTest)
+unittest.TextTestRunner(verbosity=2).run(suite)
