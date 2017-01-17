fc278a3e37e6929bcd39b26f3afd2425616ae0eb
Ani Sledge <ani.sledge@gmail.com>
2017-01-16 18:53:33 -0700

    Adds the Post Class for Google Datastore
    Adds a class for blog posts to the model module, which
    contains object relational mapping classes. Blog posts
    can be persisted to the database.
    
    * Inherits from Google App Engine's Model class
    * Contains subject, content, created, and last_modified columns
    * Has a many-to-one relationship with the User class
    * Auto-increment id column

--- model.py
+++ model.py
@@ -4,3 +4,11 @@ class User(db.Model):
 	username = db.StringProperty(required = True)
 	password = db.StringProperty(required = True)
 	email = db.StringProperty(required = False)
+
+class Post(db.Model):
+	subject = db.StringProperty(required = True)
+	content = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+	last_modified = db.DateTimeProperty(auto_now = True)
+
+	author = db.ReferenceProperty(User, collection_name='posts')
